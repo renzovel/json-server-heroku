37496badfa4e43c30ffb0566e28a79bbcb8860cc
Renzo Veliz <renzo.veliz1br@gmail.com>
2022-06-29 12:27:16 -0300

    Implementados os cors para o header solicitude

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const jsonServer= require('json-server');
+const cors=require('cors');
 const server = jsonServer.create();
 const router = jsonServer.router('database.json');
 const port = process.env.PORT||3002;
 const middlewares = jsonServer.defaults();
+server.use(cors());
 server.use(router);
 
 server.listen(port);
