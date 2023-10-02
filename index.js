4fabd23f79d8e726b7d8cffed6341050234fa4a8
Renzo Veliz <renzo.veliz1br@gmail.com>
2023-10-02 12:57:13 -0300

    Update Configuracão para vercel

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const jsonServer= require('json-server');
 const cors=require('cors');
 const server = jsonServer.create();
-const router = jsonServer.router('database.json');
 const port = process.env.PORT||3002;
+const path = require('path');
+const router = jsonServer.router(path.join(__dirname, 'database.json'));
 const middlewares = jsonServer.defaults();
 server.use(cors());
 server.use(router);
-
 server.listen(port);
