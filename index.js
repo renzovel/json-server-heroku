9ac25368876f58e9eedc7ae6f1b8f335aefaca33
Renzo Veliz <renzo.veliz1br@gmail.com>
2023-11-26 16:15:11 -0300

    new route

--- index.js
+++ index.js
@@ -6,5 +6,16 @@ const path = require('path');
 const router = jsonServer.router(path.join(__dirname, 'database.json'));
 const middlewares = jsonServer.defaults();
 server.use(cors());
+
+server.get("/sse",(req, res)=>{
+    res.setHeader("Content-Type", "text/event-stream");
+    res.setHeader("Cache-Control", "no-cache");
+    res.setHeader("Connection","keep-alive");
+    setInterval(() => {
+        const data = { mensaje: 'Hola desde el servidor.' };
+        res.write(`data: ${JSON.stringify(data)}\n\n`);
+    }, 5000);
+});
+
 server.use(router);
 server.listen(port);
